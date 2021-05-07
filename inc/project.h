63affc245fab7dc0181c6d026c8e9a75c77adfca
rjf12138 <3161173206@qq.com>
2021-05-07 23:40:21 +0800

    2021年 05月 07日 星期五 23:40:21 CST

--- inc/project.h
+++ inc/project.h
@@ -2,17 +2,26 @@
 #define __PROJECT_H__
 
 #include "basic_head.h"
+#include "json/wejson.h"
+
+using namespace my_protocol;
 
 class Project {
 public:
     Project(string name, string path);
     ~Project(void);
 
+    int load_project(string project_path);
     int create_project_dir(void);
-    int generate_config(void);
+    int generate_project_config(string path);
+    int generate_vscode_config(string path);
 private:
     string name_;
     string path_;
+    string project_path_;
+    string output_bin_path_;
+    string output_lib_path_;
+    WeJson config_;
 };
 
 #endif
--- src/project.cc
+++ src/project.cc
@@ -1,14 +1,11 @@
 #include "project.h"
 #include "exe_shell.h"
 #include "system_utils.h"
-#include "json/wejson.h"
-
-using namespace my_protocol;
 
 Project::Project(string name, string path)
 :name_(name), path_(path)
 {
-
+    project_path_ = path_ + "/" + name_;
 }
 
 Project::~Project(void)
@@ -21,90 +18,157 @@ Project::create_project_dir(void)
 {
     string result;
 
-    exe_shell_cmd(result, "mkdir %s/%s/", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/config", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/doc", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/inc", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/lib", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/lib/debug", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/lib/release", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/main", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/src", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/debug", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/debug/bin", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/debug/lib", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/release", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/release/bin", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/output/release/lib", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/.proj_config", path_.c_str(), name_.c_str());
-    exe_shell_cmd(result, "mkdir %s/%s/.vscode", path_.c_str(), name_.c_str());
+    exe_shell_cmd(result, "mkdir %s/", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/config_", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/doc", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/inc", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/lib", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/lib/debug", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/lib/release", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/main", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/src", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/debug", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/debug/bin", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/debug/lib", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/release", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/release/bin", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/output/release/lib", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/.proj_config", project_path_.c_str());
+    exe_shell_cmd(result, "mkdir %s/.vscode", project_path_.c_str());
+
+    string project_config_path = project_path_ + "/project_config.json";
+    this->generate_project_config(project_config_path);
 
     return 0;
 }
 
-int Project::generate_config(void)
+int Project::generate_project_config(string path)
 {
     string result;
     ByteBuffer buffer;
-    WeJson config("{}"), arr("[]");
+    WeJson arr("[]");
 
-    config["project_name"] = name_;
-    config["project_path"] = path_ + "/" + name_;
+    config_["project_name"] = name_;
+    config_["project_path"] = path_ + "/" + name_;
 
-    exe_shell_cmd(result, "uuidgen | awk -F[\\n] '{print $1}'");
-    config["project_uuid"] = result;
+    exe_shell_cmd(result, "uuidgen | tr -d '\n'");
+    config_["project_uuid"] = result;
 
     arr.parse("[]");
     arr.add("./lib/debug/");
     arr.add("./lib/release/");
     arr.add("./output/debug/lib/");
     arr.add("./output/release/lib/");
-    config["library_dir"] = arr;
+    config_["library_dir"] = arr;
     arr.clear();
 
     arr.parse("[]");
     arr.add("./src/");
     arr.add("./main/");
-    config["src_dir"] = arr;
+    config_["src_dir"] = arr;
     arr.clear();
 
     arr.parse("[]");
     arr.add("./inc/");
-    config["inc_dir"] = arr;
+    config_["inc_dir"] = arr;
     arr.clear();
 
     arr.parse("[]");
     arr.add("g++");
-    config["choose_compiler"] = arr;
+    config_["choose_compiler"] = arr;
     arr.clear();
 
-    config["release_compile_option"] = "-O2 -Wall -std=c++11";
+    config_["release_compile_option"] = "-O2 -Wall -std=c++11";
     arr.parse("[]");
     arr.add("gmock");
     arr.add("gtest");
-    config["release_link_library"] = arr;
+    config_["release_link_library"] = arr;
     arr.clear();
 
-    config["debug_compile_option"] = "-O0 -Wall -g -ggdb -std=c++11";
+    config_["debug_compile_option"] = "-O0 -Wall -g -ggdb -std=c++11";
     arr.parse("[]");
     arr.add("gmock_main");
     arr.add("gtest_main");
-    config["debug_link_library"] = arr;
+    config_["debug_link_library"] = arr;
     arr.clear();
 
-    config["compile_method"] = "debug";
-    config["program_run_arg"] = "";
+    config_["compile_method"] = "debug";
+    config_["program_run_arg"] = "";
 
 
-    config["compiler"] = "g++";
-    config["main_cpp_file"] = "";
-    config["generate_file_type"] = "exe";
+    config_["compiler"] = "g++";
+    config_["main_cpp_file"] = "";
+    config_["generate_file_type"] = "exe";
 
-    buffer.write_string(config.format_json());
+    buffer.write_string(config_.format_json());
     system_utils::Stream project_config;
-    project_config.open("./project_config.json");
+    project_config.open(path);
     project_config.write(buffer, buffer.data_size());
 
     return 0;
+}
+
+int Project::generate_vscode_config(string path)
+{
+    string result;
+
+    exe_shell_cmd(result, "echo \"{\"                                                                > %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    // See https://go.microsoft.com/fwlink/?LinkId=733558\"        >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    // for the documentation about the tasks.json format\"         >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"        \"tasks\": [\"                                             >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            {\"                                                    >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"type\": \"shell\",\"                             >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"label\": \"compile script\",\"                   >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"command\": \"project\",\"                        >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"args\": [\"                                      >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"-r\"\"                                       >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                ],\"                                               >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"options\": {\"                                   >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"cwd\": \"%s\"\"                              >> %s/tasks.json", path.c_str(), project_path_.c_str());
+    exe_shell_cmd(result, "echo \"                },\"                                               >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                \"group\": {\"                                     >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"kind\": \"build\",\"                         >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"isDefault\": true\"                          >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                }\"                                                >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            }\"                                                    >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"        ],\"                                                       >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"        \"version\": \"2.0.0\"\"                                   >> %s/tasks.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    }\"                                                            >> %s/tasks.json", path.c_str());
+
+    JsonString program_arg = config_["program_run_arg"];
+    JsonString program_main_file = config_["main_cpp_file"];
+    exe_shell_cmd(result, "echo \"{\"                                                                > %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"   // Use IntelliSense to learn about possible attributes.\"       >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"   // Hover to view descriptions of existing attributes.\"         >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"   // For more information, visit: \"                              >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"   // https://go.microsoft.com/fwlink/?linkid=830387\"             >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    \"version\": \"0.2.0\",\"                                      >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    \"configurations\": [\"                                        >> %s/launch.json", path.c_str()); 
+    exe_shell_cmd(result, "echo \"        {\"                                                        >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"name\": \"gdb_debug\",\"                             >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"type\": \"cppdbg\",\"                                >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"request\": \"launch\",\"                             >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo %s | awk -F[.] '{print $1}'", program_main_file.value());
+    exe_shell_cmd(result, "echo \"            \"program\": \"%s/%s\",\"                              >> %s/launch.json", output_bin_path_.c_str(), result.c_str());
+    if (program_arg == "") {
+        exe_shell_cmd(result, "echo \"            \"args\": [],\"                                    >> %s/launch.json", path.c_str());
+    } else {
+        exe_shell_cmd(result, "echo \"            \"args\": [\"%s\"],\"                              >> %s/launch.json", path.c_str(), program_arg.value().c_str());
+    }
+    exe_shell_cmd(result, "echo \"            \"stopAtEntry\": false,\"                              >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"cwd\": \"%s\",\"                                     >> %s/launch.json", path.c_str(), project_path_.c_str());
+    exe_shell_cmd(result, "echo \"            \"environment\": [],\"                                 >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"externalConsole\": false,\"                          >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"MIMode\": \"gdb\",\"                                 >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            \"setupCommands\": [\"                                 >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                {\"                                                >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"description\": \"\",\"                       >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                    \"text\": \"-enable-pretty-printing\",\"       >> %s/launch.json", path.c_str());    
+    exe_shell_cmd(result, "echo \"                    \"ignoreFailures\": true\"                     >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"                }\"                                                >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"            ]\"                                                    >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"        }\"                                                        >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"    ]\"                                                            >> %s/launch.json", path.c_str());
+    exe_shell_cmd(result, "echo \"}\"                                                                >> %s/launch.json", path.c_str());
 }
