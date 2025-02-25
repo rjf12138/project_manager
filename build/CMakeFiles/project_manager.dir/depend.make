8e2b668494e5ad94e63bca8a8e085f983920c6c7
rjf12138 <3161173206@qq.com>
2025-02-25 23:34:33 +0800

    2025年 02月 25日 星期二 23:34:33 CST

---
+++ build/CMakeFiles/project_manager.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for project_manager.
+# This may be replaced when dependencies are built.
---
+++ main/main.cc
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main(void)
+{
+    fprintf(stdout, "Hello, world!!!\n");
+    return 0;
+}
